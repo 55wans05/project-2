240d462897167f1591ab00beb0f62a5d047ca8fa
Stephen Swanson <stephen.swanson@mailbox.org>
2023-10-14 14:06:58 -0700

    Did project

--- web/app.py
+++ web/app.py
@@ -1,14 +1,41 @@
 """
-John Doe's Flask API.
+Stephen's Flask API.
 """
-
-from flask import Flask
+import os
+import configparser
+from flask import Flask, send_from_directory, send_file, abort
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+# Stolen from project 0
+def parse_config(config_paths):
+    config_path = None
+    for f in config_paths:
+        if os.path.isfile(f):
+            config_path = f
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+@app.errorhandler(404)
+def error404(error):
+    return send_file('pages/404.html'), 404
+
+@app.errorhandler(403)
+def error403(error):
+    return send_file('pages/403.html'), 403
+
+@app.route("/<path:path>")
+def hello(path):
+    if ".." in path or '~' in path:
+        abort(403)
+    return send_from_directory('pages', path)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    config = parse_config(["credentials.ini", "default.ini"])
+    app.run(debug=config["SERVER"]["DEBUG"], host='0.0.0.0', port=config["SERVER"]["PORT"])
