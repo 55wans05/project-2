c261deee74096fb2d466db4254c3b1639c588590
Stephen Swanson <stephen.swanson@mailbox.org>
2023-10-14 13:51:07 -0700

    Made start script use random port number

--- start-podman.sh
+++ start-podman.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+PORT=$((1000 + $RANDOM % 65535))
+cp credentials.ini web/credentials.ini
 cd web
+sed -i "s/PORT=5000/PORT=$PORT/g" credentials.ini
 podman build -t project2 .
-podman run -p 5000:5000 --rm -it project2
+podman run -p $PORT:5000 --rm -it project2
