e631a8945031b038b970ba16569a4aa8e60aaa72
Stephen Swanson <stephen.swanson@mailbox.org>
2023-10-14 14:06:43 -0700

    use random port on both container and locahost

--- start-podman.sh
+++ start-podman.sh
@@ -4,4 +4,4 @@ cp credentials.ini web/credentials.ini
 cd web
 sed -i "s/PORT=5000/PORT=$PORT/g" credentials.ini
 podman build -t project2 .
-podman run -p $PORT:5000 --rm -it project2
+podman run -p $PORT:$PORT --rm -it project2
